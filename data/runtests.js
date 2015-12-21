82b83e3b1aeaeea5ba7f4701b31444af1a920912
SebastianZ <sebastianzartner@gmail.com>
2015-12-21 15:28:23 +0100

    Added advanced check for <span>s (issue #6)

--- data/runtests.js
+++ data/runtests.js
@@ -3,7 +3,12 @@ iframe.contentDocument.body.setAttribute("spellcheck", "true");
 var content = iframe.contentDocument.body.innerHTML || "";
 
 var runTest = function(testObj) {
-  var contentTest = content.match(new RegExp(testObj.regex, 'gi')) || [];
+  var contentTest = [];
+  if (testObj.check) {
+    contentTest = testObj.check(content);
+  } else {
+    contentTest = content.match(new RegExp(testObj.regex, 'gi')) || [];
+  }
   testObj.count = contentTest.length;
   self.port.emit("test", testObj);
 };
