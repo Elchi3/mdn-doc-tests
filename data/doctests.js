ab57f021e90bd1d67a9b657c145d32c9bb93296d
SebastianZ <sebastianzartner@gmail.com>
2015-12-22 15:39:46 +0100

    Allowed to list specific errors (issue #1)

--- data/doctests.js
+++ data/doctests.js
@@ -8,7 +8,7 @@ var docTests = [
     desc: "en/ -> /en-US/docs/",
     regex: /\shref=\"\/*en*\//gi,
     type: ERROR, 
-    count: 0
+    errors: []
   },
   {
     id: "emptyElem",
@@ -25,7 +25,7 @@ var docTests = [
       return matches;
     },
     type: ERROR, 
-    count: 0
+    errors: []
   },
   {
     id: "languagesMacro",
@@ -33,7 +33,7 @@ var docTests = [
     desc: "{{languages()}}",
     regex: /{{\s*languages\s*/gi,
     type: ERROR, 
-    count: 0
+    errors: []
   },
   {
     id: "emptyBrackets",
@@ -41,7 +41,7 @@ var docTests = [
     desc: "{{foo()}}",
     regex: /{{\s*[a-z]*\(\)\s*}}/gi,
     type: ERROR, 
-    count: 0
+    errors: []
   },
   {
     id: "styleAttribute",
@@ -49,7 +49,7 @@ var docTests = [
     desc: "style=",
     regex: /style=["'][a-zA-Z0-9:#!%;'\.\s\(\)\-\,]*['"]/gi,
     type: ERROR, 
-    count: 0
+    errors: []
   },
   {
     id: "nameAttribute",
@@ -57,7 +57,7 @@ var docTests = [
     desc: "name=",
     regex: /name=["'][a-zA-Z0-9:#!%;'_\.\s\(\)\-\,]*['"]/gi,
     type: ERROR, 
-    count: 0
+    errors: []
   },
   {
     id: "spanCount",
@@ -74,7 +74,7 @@ var docTests = [
       return matches;
     },
     type: ERROR, 
-    count: 0
+    errors: []
   },
   {
     id: "preWithoutClass",
@@ -82,7 +82,7 @@ var docTests = [
     desc: "<pre></pre> (no syntax highlighter)",
     regex: /(<pre(?=\s|>)(?!(?:[^>=]|=(['"])(?:(?!\1).)*\1)*?class=['"])[^>]*>[\S\s]*?<\/pre>)/gi,
     type: ERROR, 
-    count: 0
+    errors: []
   },
   {
     id: "SummaryHeading",
@@ -90,7 +90,7 @@ var docTests = [
     desc: "According to the article style guide there shouldn't be a <hx>Summary</hx> heading.",
     regex: /<h[0-6]?(?!\/)[^>]+>Summary<\/h[0-6]>/gi,
     type: ERROR, 
-    count: 0
+    errors: []
   },
   {
     id: "jsRefWithParams",
@@ -98,7 +98,7 @@ var docTests = [
     desc: "Paremeters are obsolete now, e.g. {{JSRef('Global_Objects', 'Math')}}",
     regex: /{{s*JSRef\(s*/gi,
     type: ERROR, 
-    count: 0
+    errors: []
   },
   {
     id: "ExampleColonHeading",
@@ -106,7 +106,7 @@ var docTests = [
     desc: "<h3>Example: Foobar</h3> just use <h3>Foobar</h3>",
     regex: /<h[0-6]?(?!\/)[^>]+>Example:.*?<\/h[0-6]>/gi,
     type: ERROR, 
-    count: 0
+    errors: []
   },
   {
     id: "alertPrintInCode",
@@ -124,9 +124,8 @@ var docTests = [
 
       return matches;
     },
-    regex: "",
     type: ERROR, 
-    count: 0
+    errors: []
   },
   {
     id: "htmlComments",
@@ -134,7 +133,7 @@ var docTests = [
     desc: "HTML comments are not visible in wysiwyg mode and in reading mode. Not meant to comment the documentation",
     regex: /<!--[\s\S]*?-->/gi,
     type: ERROR, 
-    count: 0
+    errors: []
   },
   {
     id: "fontElement",
@@ -142,7 +141,7 @@ var docTests = [
     desc: "Using <font> elements is obsolete. Either the tag should be removed completely or replaced by CSS.",
     regex: /<font.*?>/gi,
     type: ERROR, 
-    count: 0
+    errors: []
   },
   {
     id: "httpLinks",
@@ -150,7 +149,7 @@ var docTests = [
     desc: "URLs to external resources should use HTTPS when possible",
     regex: /<a[^>]+href="http:\/\//gi,
     type: WARNING, 
-    count: 0
+    errors: []
   },
   {
     id: "macroSyntaxError",
@@ -203,7 +202,7 @@ var docTests = [
       return errors;
     },
     type: ERROR,
-    count: 0
+    errors: []
   },
   {
     id: "wrongHighlightedLine",
