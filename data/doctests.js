214900eb41b6d919a600c58ead0d6bd903eba2c2
SebastianZ <sebastianzartner@gmail.com>
2015-12-30 11:38:37 +0100

    Turned regex strings into literals (issue #28)

--- data/doctests.js
+++ data/doctests.js
@@ -3,42 +3,42 @@ var docTests = [
     id: "oldURLs",
     name: "Old 'en/' URLs",
     desc: "en/ -> /en-US/docs/",
-    regex: "\\shref=\"\\/*en*\/",
+    regex: /\shref=\"\/*en*\//gi,
     count: 0
   },
   {
     id: "emptyElem",
     name: "Empty elements",
     desc: "E.g. <p></p>",
-    regex: "(<(?!\\/)[^>]+>)+([\\s]*?)(<\\/[^>]+>)+",
+    regex: /(<(?!\/)[^>]+>)+([\s]*?)(<\/[^>]+>)+/gi,
     count: 0
   },
   {
     id: "languagesMacro",
     name: "Languages macro",
     desc: "{{languages()}}",
-    regex: "{{\\s*languages\\s*",
+    regex: /{{\s*languages\s*/gi,
     count: 0
   },
   {
     id: "emptyBrackets",
     name: "Empty brackets",
     desc: "{{foo()}}",
-    regex: "{{\\s*[a-z]*\\(\\)\\s*}}",
+    regex: /{{\s*[a-z]*\(\)\s*}}/gi,
     count: 0
   },
   {
     id: "styleAttribute",
     name: "Style attributes",
     desc: "style=",
-    regex: "style=[\"'][a-zA-Z0-9:#!%;'\\.\\s\\(\\)\\-\\,]*['\"]",
+    regex: /style=["'][a-zA-Z0-9:#!%;'\.\s\(\)\-\,]*['"]/gi,
     count: 0
   },
   {
     id: "nameAttribute",
     name: "Name attributes",
     desc: "name=",
-    regex: "name=[\"'][a-zA-Z0-9:#!%;'_\\.\\s\\(\\)\\-\\,]*['\"]",
+    regex: /name=["'][a-zA-Z0-9:#!%;'_\.\s\(\)\-\,]*['"]/gi,
     count: 0
   },
   {
@@ -61,28 +61,28 @@ var docTests = [
     id: "preWithoutClass",
     name: "&lt;pre&gt; w/o class",
     desc: "<pre></pre> (no syntax highlighter)",
-    regex: "(<pre(?=\\s|>)(?!(?:[^>=]|=(['\"])(?:(?!\\1).)*\\1)*?class=['\"])[^>]*>[\\S\\s]*?<\\/pre>)",
+    regex: /(<pre(?=\s|>)(?!(?:[^>=]|=(['"])(?:(?!\1).)*\1)*?class=['"])[^>]*>[\S\s]*?<\/pre>)/gi,
     count: 0
   },
   {
     id: "SummaryHeading",
     name: "Summary heading",
     desc: "According to the article style guide there shouldn't be a <hx>Summary</hx> heading.",
-    regex: "<h[0-6]?(?!\\/)[^>]+>Summary<\\/h[0-6]>",
+    regex: /<h[0-6]?(?!\/)[^>]+>Summary<\/h[0-6]>/gi,
     count: 0
   },
   {
     id: "jsRefWithParams",
     name: "JSRef params",
     desc: "Paremeters are obsolete now, e.g. {{JSRef('Global_Objects', 'Math')}}",
-    regex: "{{s*JSRef\\(s*",
+    regex: /{{s*JSRef\(s*/gi,
     count: 0
   },
   {
     id: "ExampleColonHeading",
     name: "'Example:' heading",
     desc: "<h3>Example: Foobar</h3> just use <h3>Foobar</h3>",
-    regex: "<h[0-6]?(?!\\/)[^>]+>Example:.*?<\\/h[0-6]>",
+    regex: /<h[0-6]?(?!\/)[^>]+>Example:.*?<\/h[0-6]>/gi,
     count: 0
   },
   {
@@ -108,14 +108,14 @@ var docTests = [
     id: "htmlComments",
     name: "HTML comments",
     desc: "HTML comments are not visible in wysiwyg mode and in reading mode. Not meant to comment the documentation",
-    regex: "<!--[\\s\\S]*?-->",
+    regex: /<!--[\s\S]*?-->/gi,
     count: 0
   },
   {
     id: "fontElement",
     name: "&lt;font&gt; element",
     desc: "Using <font> elements is obsolete. Either the tag should be removed completely or replaced by CSS.",
-    regex: "<font.*?>",
+    regex: /<font.*?>/gi,
     count: 0
   }
 ];
