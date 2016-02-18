9360df80eecdcc236d5e9a08fe742429fee6c86b
Florian Scholz <elchi3@elchi3.de>
2016-02-18 18:53:23 +0100

    Indicate test results via a button in reading mode
    This adds a ToggleButton to the add-on.
    In reading mode, an XHR request is made to the raw
    version of a page. The button then shows the total
    number of errors and warnings from testing against
    the raw version. Clicking the button shows the
    detailed results in the sidebar as usual.
    When editing, the button is disabled, but will
    show the result number as visual help.

--- data/runtests.js
+++ data/runtests.js
@@ -1,9 +1,33 @@
-let iframe = document.querySelectorAll("iframe.cke_wysiwyg_frame")[0];
-let content = "";
-let rootElement = null;
+var rootElement = null;
+var mode = self.options.mode;
+var tabURL = self.options.tabURL;
+var errorCount;
+
+// Use CKEditor source when editing
+if (mode === "editing") {
+  var iframe = document.querySelectorAll("iframe.cke_wysiwyg_frame")[0];
 if (iframe) {
   iframe.contentDocument.body.setAttribute("spellcheck", "true");
   rootElement = iframe.contentDocument.body;
+}
+// Use ?raw page source when reading
+} else if (mode.indexOf("reading") !== -1) {
+  var xhr = new XMLHttpRequest();
+  var url = tabURL.split('#')[0] + "?raw";
+  xhr.open("GET", url , true);
+  xhr.addEventListener("load", function(event) {
+    var domParser = new DOMParser();
+    var doc = domParser.parseFromString(xhr.responseText, "text/html");
+    rootElement = doc.body;
+    // Don't run the test twice when using the sidebar "runTest" button
+    if (mode !== "reading-with-sidebar") {
+      errorCount = 0;
+      for (var prop in docTests) {
+        runTest(docTests[prop], prop);
+      }
+    }
+  }, false);
+  xhr.send();
 }
 
 let runTest = function(testObj, id) {
@@ -13,6 +37,7 @@ let runTest = function(testObj, id) {
     let contentTest = testObj.check(rootElement);
     testObj.errors = contentTest;
     self.port.emit("processTestResult", testObj, id);
+    self.port.emit("badgeUpdate", errorCount);
   }
 };
 
@@ -22,21 +47,24 @@ self.port.on("runTests", function() {
   }
 });
 
-let btns = document.querySelectorAll(".btn-save, .btn-save-and-edit");
-let comment = document.querySelector("#page-comment #id_comment");
+// Disable save buttons if no revision comment has been entered
+if (mode === "editing") {
+  let btns = document.querySelectorAll(".btn-save, .btn-save-and-edit");
+  let comment = document.querySelector("#page-comment #id_comment");
 
-let disableBtns = function(bool) {
-  for (let i = 0; i < btns.length; i++) {
-    btns[i].disabled = bool;
+  let disableBtns = function(bool) {
+    for (let i = 0; i < btns.length; i++) {
+      btns[i].disabled = bool;
+    }
+  };
+
+  if (comment) {
+    if (comment.value === '') {
+      disableBtns(true);
+    }
+
+    comment.addEventListener("change", function() {
+      disableBtns(false);
+    });
   }
-};
-
-if (comment) {
-  if (comment.value === '') {
-    disableBtns(true);
-  }
-
-  comment.addEventListener("change", function() {
-    disableBtns(false);
-  });
 }
