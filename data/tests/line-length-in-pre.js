01d372949e09ceb4b9ee9ccb5057d70d41719889
SebastianZ <sebastianzartner@gmail.com>
2016-02-23 15:19:20 +0100

    Only check line length of plain text in code samples (fixes #113)

--- data/tests/line-length-in-pre.js
+++ data/tests/line-length-in-pre.js
@@ -9,6 +9,10 @@ docTests.lineLengthInPre = {
       // While editing it happens that there are <br>s added instead of line break characters
       // Those need to be replaced by line breaks to correctly recognize long lines
       var codeBlock = pres[i].innerHTML.replace(/<br\/?>/g, "\n");
+
+      // Remove all other HTML tags and only display the plain text
+      var codeBlock = codeBlock.replace(/<.+?>/g, "");
+
       var longLines = codeBlock.match(/^(?:[^\r\n]|\r(?!\n)){78,}$/gm);
       if (longLines) {
         matches = matches.concat(longLines);
