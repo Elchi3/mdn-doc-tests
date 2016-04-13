d23b69544c5c8c742fe3881dd46deadfeea984b5
SebastianZ <sebastianzartner@gmail.com>
2016-04-13 12:04:46 +0200

    Added test for different locale links (issue #138)

---
+++ data/tests/different-locale-links.js
@@ -0,0 +1,25 @@
+docTests.differentLocaleLinks = {
+  name: "different_locale_links",
+  desc: "different_locale_links_desc",
+  check: function checkDifferentLocaleLinks(rootElement) {
+    var [, pageDomain, pageLocale] = document.URL.match(/^(?:https?:\/\/)(.+?)\/([^\/]+)/i) ||
+        ["", "developer.mozilla.org", "en-US"];
+    var links = rootElement.getElementsByTagName("a");
+    var matches = [];
+    for (var i = 0; i < links.length; i++) {
+      var href = links[i].getAttribute("href");
+      if (href) {
+        var [, linkDomain, linkLocale] = href.match(/^(?:https?:\/\/(.+?))?\/([^\/]+)/i);
+        if ((!linkDomain || linkDomain === pageDomain) && linkLocale !== pageLocale)
+        matches.push({
+          msg: "link_using_wrong_locale",
+          msgParams: [href, pageLocale]
+        });
+      }
+    }
+
+    return matches;
+  },
+  type: ERROR,
+  errors: []
+};
