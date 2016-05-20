d508e06e6fae2f7e950411c70e7698e4169dd243
SebastianZ <sebastianzartner@gmail.com>
2016-05-20 14:50:56 +0200

    Added unit test for article length test (issue #17)

---
+++ test/test-article-length.js
@@ -0,0 +1,40 @@
+const {url, runTests} = require("./testutils");
+
+exports["test doc articleLength"] = function testArticleLength(assert, done) {
+  const tests = [
+    {
+      str: Array(100).fill("foo").join(" "),
+      expected: [
+        {
+          msg: "article_length_info",
+          msgParams: ["100", "< 1"]
+        }
+      ]
+    },
+    {
+      str: Array(500).fill("foo").join(" "),
+      expected: [
+        {
+          msg: "article_length_info",
+          msgParams: ["500", "2"]
+        }
+      ]
+    },
+    {
+      str: Array(2000).fill("foo").join(" "),
+      expected: [
+        {
+          msg: "article_length_info",
+          msgParams: ["2000", "7"]
+        },
+        {
+          msg: "long_article"
+        }
+      ]
+    }
+  ];
+
+  runTests(assert, done, "articleLength", "article length", url, tests);
+};
+
+require("sdk/test").run(exports);
