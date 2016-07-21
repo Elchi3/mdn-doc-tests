ee9027a2fb1802986dc49eb83c2b3f4cb6ef254a
SebastianZ <sebastianzartner@gmail.com>
2016-07-21 15:24:00 +0200

    Skip unavailable tests in unit tests (fixes #184)

--- test/testrunner.js
+++ test/testrunner.js
@@ -1,13 +1,15 @@
 // This test runner is called from unit tests only
 
 let testObj = docTests[self.options.name];
-let rootElement = document.createElement('div');
+let rootElement = document.createElement("div");
 let tests = JSON.parse(self.options.tests);
 
 tests.forEach(test => {
   rootElement.innerHTML = test.str;
-  let matches = testObj.check(rootElement);
-  testObj.errors = matches;
-  testObj.expected = test.expected;
-  self.port.emit('processTestResult', testObj, self.options.name);
+  if (testObj) {
+    let matches = testObj.check(rootElement);
+    testObj.errors = matches;
+    testObj.expected = test.expected;
+  }
+  self.port.emit("processTestResult", testObj, self.options.name);
 });
